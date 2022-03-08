693d013aeb1e7bd6a09deeab7c8e2cc935a67bfb
Ryan Raymundo <90113061+RyanRaymundo99@users.noreply.github.com>
2022-03-08 15:17:20 -0800

    enter

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -1,10 +1,35 @@
 import React from 'react';
 
+import { images } from '../../constants'
+
 import './AboutUs.css';
 
 const AboutUs = () => (
-  <div>
-    AboutUs
+  <div className='app__aboutus app__bg flex__center section__padding' id='about'>
+    <div className='app__aboutus-overlay flex__center'>
+      <img src={images.G} alt="G letter" />
+    </div>
+
+    <div className='app__aboutus-content flex__center'>
+      <div className='app__aboutus-content_about'>
+        <h1 className='headtext__cormorant'>About us</h1>
+        <img src={images.spoon} alt="About spoon" className='spoon__img' />
+        <p className='p__opensans'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus fugiat sequi iusto quia autem. Fugiat ducimus doloribus beatae, error expedita explicabo molestias tempore, eveniet sapiente corporis harum debitis delectus quibusdam.</p>
+        <button type='buttton' className='custom__button'>know more</button>
+      </div>
+
+      <div className='app__aboutus-content_knife flex__center'>
+        <img src={images.knife} alt="about us knife" />
+      </div>
+
+      <div className='app__aboutus-content_history'>
+        <h1 className='headtext__cormorant'>Our history</h1>
+        <img src={images.spoon} alt="About spoon" className='spoon__img' />
+        <p className='p__opensans'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus fugiat sequi iusto quia autem. Fugiat ducimus doloribus beatae, error expedita explicabo molestias tempore, eveniet sapiente corporis harum debitis delectus quibusdam.</p>
+        <button type='buttton' className='custom__button'>know more</button>
+      </div>
+      
+    </div>
   </div>
 );
 
--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 
+import {images} from '../../constants'
+import {SubHeading} from '../../components'
 import './Header.css';
 
 const Header = () => (
-  <div>
-    Header
+  <div className='app__header app__wrapper section__padding' id='home'>
+    <div className='app__wrapper_info'>
+      <SubHeading title="chase the new flavors" />
+      <h1 className='app__header-h1'> The key to fine Dining</h1>
+      <p className='p__opensans' style={{margin: '2rem 0'}}> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Et obcaecati blanditiis expedita architecto fugiat dolor vitae placeat a atque quam? Cum et reiciendis omnis saepe assumenda eligendi dolorum aliquid quam.</p>
+      <button type='button' className='custom__button'>Explore Menu</button>
+    </div>
+
+    <div className='app__wrapper_img'>
+      <img src={images.welcome} alt="Header image" />
+    </div>
   </div>
 );
 
