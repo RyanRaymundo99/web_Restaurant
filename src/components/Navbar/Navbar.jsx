cb29a45f94d33c37469ca2704ffbc960ee59540e
Ryan Raymundo <90113061+RyanRaymundo99@users.noreply.github.com>
2022-03-14 18:23:16 -0700

    enter

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -14,11 +14,11 @@ const Navbar = () => {
       <img src={images.gericht} alt="app logo" />
     </div>
     <ul className='app__navbar-links'>
-      <li className='p__opensans'><a href="home">Home</a></li>
-      <li className='p__opensans'><a href="about">About</a></li>
-      <li className='p__opensans'><a href="menu">Menu</a></li>
-      <li className='p__opensans'><a href="awards">Awards</a></li>
-      <li className='p__opensans'><a href="contact">Contact</a></li>
+      <li className='p__opensans'><a href="#home">Home</a></li>
+      <li className='p__opensans'><a href="#about">About</a></li>
+      <li className='p__opensans'><a href="#menu">Menu</a></li>
+      <li className='p__opensans'><a href="#awards">Awards</a></li>
+      <li className='p__opensans'><a href="#contact">Contact</a></li>
     </ul>
     <div className='app__navbar-login'>
       <a href="login" className='p__opensans'>Login / Register</a>
